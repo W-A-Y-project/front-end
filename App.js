7a12e8662a74f2497ceb8ba0c333860afb698bcd
Rafaela Ferreira <142462851+cc23015@users.noreply.github.com>
2024-06-04 15:05:12 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './src/screens/Login';
 import Cadastro from './src/screens/Cadastro';
-//import Cadastro from './src/screens/Cadastro';
+import Feed from './src/screens/Feed';
+
 
 const Stack = createStackNavigator();
 
@@ -12,8 +13,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name = "" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name = "Login" component={Login} />
+        <Stack.Screen name = "Cadastro" component={Cadastro} />
+        <Stack.Screen name = "Feed" component={Feed}/>
         
       </Stack.Navigator>
     </NavigationContainer>
