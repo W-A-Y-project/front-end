ae061f259d5fb33c6249ca452a92ef95cccfb052
Rafaela Ferreira <142462851+cc23015@users.noreply.github.com>
2024-06-03 22:47:16 -0300

    Update login.js
    mudança no box de email e senha

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,19 +1,22 @@
+import React, { useState } from "react";
+import { SafeAreaView, View, ScrollView, Text, TextInput, TouchableOpacity, Alert } from "react-native";
 
-import React from "react";
-import { SafeAreaView, View, ScrollView, Image, Text, TouchableOpacity, Alert } from "react-native";
+import { Entrada, divider, explanation, button, NativeScreen } from "../styles/styles"; // Ajuste nos imports
+import { cadastroRedirect, verifyLogin } from '../functions/functions'; // Importação da função verifyLogin
 
-import {Entrada, divider, explanation, button, NativeScreen } from "../styles/styles"; // Ajuste nos imports
-import {cadastroRedirect} from './functions/functions';
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isEmailFocused, setEmailFocused] = useState(false);
+  const [isPasswordFocused, setPasswordFocused] = useState(false);
 
-const Login =({navigation}) => {
+
   return (
-      <SafeAreaView style={NativeScreen.safeAreaView}>
-          <ScrollView style={NativeScreen.scrollView}>
-              <View style={NativeScreen.View}>
-                
-                  
-              </View>
-
+    <SafeAreaView style={NativeScreen.safeAreaView}>
+      <ScrollView style={NativeScreen.scrollView}>
+        <View style={NativeScreen.View}>
+          {/* Adicione outros componentes aqui se necessário */}
+        </View>
               <Text></Text>
               <Text></Text>
               <Text></Text>      
@@ -30,60 +33,73 @@ const Login =({navigation}) => {
                   {"Entre usando seu CPF e senha!"}
               </Text>
 
-              {/* input de CPF */}
-              <View style={Entrada.inputBox}>
-                  <Text style={Entrada.inputText}>
-                      {"000.000.000-00"}
-                  </Text>
-              </View>
-              <View style={Entrada.inputBox}>
-                  <Text style={Entrada.inputText}>
-                      {"senha"}
-                  </Text>
-              </View>
-              {/* input de senha */}
+        {/* input de e-mail */}
+        <View style={Entrada.inputBox}>
+          <TextInput
+            style={Entrada.inputText}
+            placeholder={isEmailFocused ? '' : 'email@example.com'}
+            value={email}
+            onFocus={() => setEmailFocused(true)}
+            onBlur={() => setEmailFocused(false)}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
 
-              {/* BOTÃO DE ENTRAR */}
-              <TouchableOpacity onPress={() => Alert.alert("Entrando...")} style={button.darkButton}>
-                  <Text style={button.text}>
-                      {"ENTRAR"}
-                  </Text>
-              </TouchableOpacity>
-              {/* FIM DO BOTÃO DE ENTRAR */}
+        {/* input de senha */}
+        <View style={Entrada.inputBox}>
+          <TextInput
+            style={Entrada.inputText}
+            placeholder={isPasswordFocused ? '' : 'senha'}
+            value={password}
+            onFocus={() => setPasswordFocused(true)}
+            onBlur={() => setPasswordFocused(false)}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </View>
 
+        {/* BOTÃO DE ENTRAR */}
+        <TouchableOpacity onPress={() => FeedRedirect} style={button.darkButton}>
+          <Text style={button.text}>
+            {"ENTRAR"}
+          </Text>
+        </TouchableOpacity>
+        {/* FIM DO BOTÃO DE ENTRAR */}
 
-              <View style={{ flexDirection: "row", alignItems: "center", marginHorizontal: 24, marginBottom: 23 }}>
-                <View style={{ flex: 1, height: 1, backgroundColor: "#363851" }} />
-                <Text style={{ marginHorizontal: 10, color: "#363851" }}>ou</Text>
-                <View style={{ flex: 1, height: 1, backgroundColor: "#363851" }} />
-              </View>
+        <View style={{ flexDirection: "row", alignItems: "center", marginHorizontal: 24, marginBottom: 23 }}>
+          <View style={{ flex: 1, height: 1, backgroundColor: "#363851" }} />
+          <Text style={{ marginHorizontal: 10, color: "#363851" }}>ou</Text>
+          <View style={{ flex: 1, height: 1, backgroundColor: "#363851" }} />
+        </View>
 
-          {/* botão de não tenho cadastro */}
-          <TouchableOpacity onPress={() => cadastroRedirect(navigation)} style={button.clearButton}>
-            <Text style={button.clearText}>
-              {"não tenho cadastro!"}
-            </Text>
-          </TouchableOpacity>
-          {/* fim do botão */}
+        {/* botão de não tenho cadastro */}
+        <TouchableOpacity onPress={() => cadastroRedirect(navigation)} style={button.clearButton}>
+          <Text style={button.clearText}>
+            {"não tenho cadastro!"}
+          </Text>
+        </TouchableOpacity>
+        {/* fim do botão */}
 
-              <Text
-                  style={{
-                      fontSize: 12,
-                      marginBottom: 202,
-                      marginHorizontal: 32,
-                      width: 311,
-                  }}>
-                  {"By clicking continue, you agree to our Terms of Service and Privacy Policy"}
-              </Text>
-              <View
-                  style={{
-                      backgroundColor: "#000000",
-                      borderRadius: 100,
-                      marginHorizontal: 120,
-                  }}>
-              </View>
-          </ScrollView>
-      </SafeAreaView>
+        <Text
+          style={{
+            fontSize: 12,
+            marginBottom: 202,
+            marginHorizontal: 32,
+            width: 311,
+          }}>
+          {"By clicking continue, you agree to our Terms of Service and Privacy Policy"}
+        </Text>
+        <View
+          style={{
+            backgroundColor: "#000000",
+            borderRadius: 100,
+            marginHorizontal: 120,
+          }}>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
