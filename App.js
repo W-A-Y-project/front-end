c123525600aa782d8de4ff974068304ce0ec9bb2
Raphael da Silva Santos <raapphaaeell@gmail.com>
2024-08-19 18:13:07 -0300

    conect login route

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './src/screens/Login';
+import Login from './src/screens/login';
 import Cadastro from './src/screens/Cadastro';
 import Feed from './src/screens/Feed';
 
